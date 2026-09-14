0d28715e8032bb6263f969f09ec3283bf4f6b4b5
Ahmed-Gomaa-Mo-CS <ahmedgomaamo000@gmail.com>
2026-09-14 17:56:03 +0300

    Implement MessageBus class for message handling

---
+++ aegis/coordination/message_bus.py
@@ -0,0 +1,22 @@
+
+class MessageBus:
+
+    def __init__(self):
+        self.messages = []
+
+    def broadcast(self, sender, message_type, content):
+
+        msg = {
+            "from": sender,
+            "type": message_type,
+            "content": content
+        }
+
+        self.messages.append(msg)
+
+    def get_messages(self):
+        return self.messages
+
+    def clear(self):
+        self.messages = []
+
